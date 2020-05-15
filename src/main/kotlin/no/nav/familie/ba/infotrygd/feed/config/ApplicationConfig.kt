37dd5df41d1ab3bca60503515cfa3641616a6a42
Martine Enger <martine.enger@nav.no>
2020-05-15 08:26:54 +0200

    Autentisering og swagger (#1)
    * La til validering av sikkerhetstoken. Lagt til config for preprod og prod, enablet tokenvalidering og tatt i bruk nav-security-test-biblioteket lokalt. Ignorer swagger i tokenvalideringen
    
    Co-authored-by: Marcus Gustafson <marcus.gustafson@nav.no>

--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/ApplicationConfig.kt
@@ -1,13 +1,17 @@
 package no.nav.familie.ba.infotrygd.feed.config
 
 import no.nav.familie.log.filter.LogFilter
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootConfiguration
+@EnableJwtTokenValidation(ignore = ["org.springframework","springfox.documentation.swagger.web.ApiResourceController"])
+@EnableSwagger2
 @ComponentScan(ApplicationConfig.pakkenavn)
 class ApplicationConfig {
 
--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/rest/InfotrygdFeedController.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/rest/InfotrygdFeedController.kt
@@ -1,24 +1,22 @@
 package no.nav.familie.ba.infotrygd.feed.rest
 
-import io.micrometer.core.annotation.Timed
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
-import org.springframework.http.HttpStatus
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
-class InfotrygdFeedController() {
+@ProtectedWithClaims(issuer = "sts")
+class InfotrygdFeedController {
 
     @Operation(
             summary = "Hent liste med hendelser.",
             description = "Henter hendelser med sekvensId større enn sistLesteSekvensId."
     )
-    @GetMapping("/api/v1/barnetrygd/feed",
-            produces = ["application/json; charset=us-ascii"])
+    @GetMapping("/api/v1/barnetrygd/feed", produces = ["application/json; charset=us-ascii"])
     fun feed(
             @Parameter(description = "Sist leste sekvensnummer.", required = true, example = "0")
             @RequestParam("sistLesteSekvensId") sekvensnummer: Long,
@@ -27,15 +25,14 @@ class InfotrygdFeedController() {
             @RequestParam("maxAntall", required = false, defaultValue = "100") maksAntall: Int,
 
             @Parameter(description = "Støttede hendelser vedtak eller fødselsmeldinger.")
-            @RequestParam("type", required = true) type: String?,
+            @RequestParam("type") type: String,
 
             @Parameter(description = "Ikke implementert. Ta kontakt med Team Familie dersom dette er ønsket.")
             @RequestParam("aktoerId", required = false) aktørId: String?
     ) : ResponseEntity<String> {
         if(aktørId != null) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "Det er ikke implementert støtte for parametrene 'aktoerId' på gjeldende tidspunkt. " +
-                            "Vennligst ta kontakt med Team Familie dersom det er behov for å implementere disse.")
+            return ResponseEntity.badRequest()
+                    .body("Det er ikke implementert støtte for parameter 'aktoerId' på gjeldende tidspunkt")
         }
 
         return ResponseEntity.ok("ok")
