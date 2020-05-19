fea9cb37e92a3cdcbb673c829ad2dac283c08eca
Marcus Gustafson <marcus.gustafson@nav.no>
2020-05-19 13:19:08 +0200

    La til to apier (opprettHenvendelse og opprettFoedsels) for å opprette fabrikerte test-data

--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/rest/TestDataController.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/rest/TestDataController.kt
@@ -1,23 +1,51 @@
 package no.nav.familie.ba.infotrygd.feed.rest
 
-import no.nav.familie.ba.infotrygd.feed.rest.dto.FeedMeldingDto
+import io.swagger.v3.oas.annotations.Parameter
+import no.nav.familie.ba.infotrygd.feed.rest.dto.Type
 import no.nav.familie.ba.infotrygd.feed.service.InfotrygdFeedService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import javax.validation.constraints.Size
 
 @RestController
 @Profile("!prod")
 @ProtectedWithClaims(issuer = "sts")
 class TestDataController(val infotrygdFeedService: InfotrygdFeedService) {
 
-    @PostMapping("/api/hendelse", consumes = ["application/json; charset=us-ascii"])
-    fun lagNyHendelse(@RequestBody hendelse: FeedMeldingDto): ResponseEntity<String> {
+    @PostMapping("/api/foedselsmelding")
+    fun lagNyFoedselsMelding(@Parameter(description = "Fødselsnummer barn.", required = true, example = "12345678910")
+                             @RequestParam("fnrBarn") fnrBarn: String,
 
-        return Result.runCatching { infotrygdFeedService.opprettNyHendelse(hendelse) }
+                             @Parameter(description = "Fødselsnummer mor.", example = "12345678910")
+                             @RequestParam("fnrMor", required = false) fnrMor: String?,
+
+                             @Parameter(description = "Fødselsnummer far.", example = "12345678910")
+                             @RequestParam("fnrfar", required = false) fnrFar: String?): ResponseEntity<String> =
+
+            opprettFeed(type = Type.BA_Foedsel_v1, fnrBarn = fnrBarn, fnrFar = fnrFar, fnrMor = fnrMor)
+
+    @PostMapping("/api/vedtakmelding")
+    fun lagNyHenvendelsesMelding(@Parameter(description = "Fødselsnummer støneds mottaker.", required = true, example = "12345678910")
+                                 @RequestParam("fnrStoenadsmottaker") fnrStoenadsmottaker: String,
+
+                                 @Parameter(description = "Fødselsnummer far.", example = "12345678910")
+                                 @RequestParam("datoStartNyBA") datoStartNyBA: LocalDate?,
+
+                                 @Parameter(description = "Fødselsnummer mor.", example = "12345678910")
+                                 @RequestParam("datoStartNyBA", required = false) fnrMor: String?,
+
+                                 @Parameter(description = "Fødselsnummer far.", example = "12345678910")
+                                 @RequestParam("fnrfar", required = false) fnrFar: String?): ResponseEntity<String> =
+
+            opprettFeed(type = Type.BA_Opphoert_v1, fnrStoenadsmottaker = fnrStoenadsmottaker, datoStartNyBA = datoStartNyBA, fnrFar = fnrFar, fnrMor = fnrMor)
+
+    private fun opprettFeed(type: Type, fnrBarn: String? = null, fnrFar: String? = null, fnrMor: String? = null, fnrStoenadsmottaker: String? = null, datoStartNyBA: LocalDate? = null): ResponseEntity<String> {
+        return Result.runCatching { infotrygdFeedService.opprettNyFeed(type = type, fnrBarn = fnrBarn, fnrFar = fnrFar, fnrMor = fnrMor, fnrStonadsmottaker = fnrStoenadsmottaker, datoStartNyBA = datoStartNyBA) }
                 .fold(onSuccess = {
                     ResponseEntity.ok("Hendelse opprettet")
                 }, onFailure = {
--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/service/InfotrygdFeedService.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/service/InfotrygdFeedService.kt
@@ -7,9 +7,13 @@ import no.nav.familie.ba.infotrygd.feed.rest.dto.InnholdFødsel
 import no.nav.familie.ba.infotrygd.feed.rest.dto.InnholdVedtak
 import no.nav.familie.ba.infotrygd.feed.rest.dto.Type
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class InfotrygdFeedService(val feedRepository: FeedRepository) {
+
+    fun opprettNyFeed(type: Type, fnrBarn: String?, fnrFar: String?, fnrMor: String?, fnrStonadsmottaker: String?, datoStartNyBA: LocalDate?) =
+        feedRepository.save(Feed(type = type,fnrBarn = fnrBarn, fnrFar = fnrFar, fnrMor = fnrMor, fnrStonadsmottaker = fnrStonadsmottaker, datoStartNyBa = datoStartNyBA))
 
     fun opprettNyHendelse(hendelse: FeedMeldingDto) {
 
