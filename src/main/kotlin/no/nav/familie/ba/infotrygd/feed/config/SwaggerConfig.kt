f9c55663e980045795229e75b723c98ec2cff8d5
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-04-06 10:12:18 +0200

    Fikser swagger config etter bump

--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/SwaggerConfig.kt
@@ -12,10 +12,8 @@ import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
-@EnableSwagger2
 class SwaggerConfig {
 
 
