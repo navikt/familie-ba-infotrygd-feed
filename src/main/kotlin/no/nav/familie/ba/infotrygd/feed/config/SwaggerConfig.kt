02fcd9404346671ea2fd3f204869fc204f873b23
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-04-06 14:45:21 +0200

    Revert "Spring, swagger"
    This reverts commit 5f537d0585cb0314058a9a29478f887d3107734f.

--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/SwaggerConfig.kt
@@ -12,10 +12,10 @@ import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 class SwaggerConfig {
 
 
