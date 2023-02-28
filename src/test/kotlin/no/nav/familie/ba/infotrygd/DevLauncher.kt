778ee4d0876ef3066eee6143dc1b733df36c4c0f
Stig Helge Strøm <stig.helge.strom@nav.no>
2023-02-28 16:15:01 +0100

    Pakke og konfigendring ved spring boot 3

--- src/test/kotlin/no/nav/familie/ba/infotrygd/DevLauncher.kt
+++ src/test/kotlin/no/nav/familie/ba/infotrygd/DevLauncher.kt
@@ -1,9 +1,11 @@
 package no.nav.familie.ba.infotrygd
 
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ba.infotrygd.feed"])
+@EnableMockOAuth2Server
 class DevLauncher
 
 fun main(args: Array<String>) {
