d8916e9c32de6d0dfc4dafecd22c9c2c688dcfda
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-11-25 15:16:08 +0100

    Fiks kompileringsfeil

--- src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/FlywayConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/feed/config/FlywayConfiguration.kt
@@ -3,7 +3,7 @@ package no.nav.familie.ba.infotrygd.feed.config
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
+import org.springframework.boot.flyway.autoconfigure.FlywayConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
